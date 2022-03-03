78734d5c2dcbeb0e38444e3b5bfd152a5011c97a
Wrokbrook repo <chukschiboy@gmail.com>
2022-03-03 19:23:48 +0100

    creating the home directory with the main food page file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home/main_food_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainFoodPage(),
     );
   }
 }
