8b7f7d307beb6711622906ec4df1d911660fc211
Wrokbrook repo <chukschiboy@gmail.com>
2022-03-30 12:42:55 +0100

    adding the const widgets and list page view builder

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
@@ -34,7 +36,15 @@ class _MainFoodPageState extends State<MainFoodPage> {
                         text: "Nigeria",
                         color: AppColors.mainColor,
                       ),
-                      Text("City"),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Lagos",
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded),
+                        ],
+                      ),
                     ],
                   ),
                   Center(
@@ -55,6 +65,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
               ),
             ),
           ),
+          FoodPageBody(),
         ],
       ),
     );
--- lib/widgets/small_text.dart
+++ lib/widgets/small_text.dart
@@ -8,7 +8,7 @@ class SmallText extends StatelessWidget {
 
   SmallText({
     Key? key,
-    this.color = const Color(0xFF89dad0),
+    this.color = const Color(0xFFccc7c5),
     required this.text,
     this.size = 12,
     this.height = 1.2,
@@ -22,6 +22,7 @@ class SmallText extends StatelessWidget {
         fontFamily: 'Roboto',
         color: color,
         fontSize: size,
+        height: height,
       ),
     );
   }
